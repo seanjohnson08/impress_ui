cf8ee60a51066e674e3326eadbbba7445e5e2ce5
Sean <smartgenius1@yahoo.com>
2013-05-31 22:59:03 -0400

    Broke out into includes

--- includes/toolbar.php
+++ includes/toolbar.php
@@ -1,5 +1,6 @@
 <div class="toolbar">
-    <input type="text" name="xpos" value="0" />
-    <input type="text" name="ypos" value="0" />
-    <input type="text" name="zpos" value="0" />
+    <input type="text" name="xpos" value="0" /><br />
+    <input type="text" name="ypos" value="0" /><br />
+    <input type="text" name="zpos" value="0" /><br />
+    <button>Add Slide</button>
 </div>
