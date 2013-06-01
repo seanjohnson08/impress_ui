ea0c72abaa3115924208b3afa947b2fb4697c4dc
Sean <smartgenius1@yahoo.com>
2013-05-31 23:00:47 -0400

    Script tags can't be self-closing. Haha

--- includes/header.php
+++ includes/header.php
@@ -1,2 +1,2 @@
-    <script type="javascript/text" src="JS/impress.js" />
-    <script type="javascript/text" src="JS/jquery-2.0.2.min.js" />
+    <script type="javascript/text" src="JS/impress.js"></script>
+    <script type="javascript/text" src="JS/jquery-2.0.2.min.js"></script>
